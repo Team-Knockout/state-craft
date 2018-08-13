8d4e00476e6f58185cd38685cd1c1e29e26c0bc8
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-13 16:09:18 -0700

    static render of results app

--- js/services/results-text.js
+++ js/services/results-text.js
@@ -1,5 +1,46 @@
-let personalFreedom = {
-    lowerHalf: 'you dont have much freedom',
-    upperHalf: 'you have lots of freedom'
 
+let personalFreedom = {
+    lowerHalf: 'you dont have much personal freedom',
+    upperHalf: 'you have lots of personal freedom'
+
+};
+
+let economicFreedom = {
+    lowerHalf: 'you dont have much economic freedom',
+    upperHalf: 'you have lots of economic freedom'
+};
+
+let politicalFreedom = {
+    lowerHalf: 'you dont have have much political freedom',
+    upperHalf: 'you have lots of political freedom'
+
+};
+
+let approval = {
+    lowerHalf: 'you dont have have much approval',
+    upperHalf: 'you have lots of approval'
+};
+
+let population = {
+    lowerHalf: 'you dont have have much population',
+    upperHalf: 'you have a large population'
+};
+
+let econProduction = {
+    lowerHalf: 'you  dont have have much economic production',
+    upperHalf: 'you have lots of economic production'
+};
+
+let health = {
+    lowerHalf: 'you  dont have have much healthcare',
+    upperHalf: 'you have lots of healthcare'
+};
+let safety = {
+    lowerHalf: 'you  dont have have much saftey',
+    upperHalf: 'you have lots of saftey'
+}; 
+
+let environment = {
+    lowerHalf: 'you  dont have have much enviromental policies',
+    upperHalf: 'you have lots of enviromental policies'
 };
