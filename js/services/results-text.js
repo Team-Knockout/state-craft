4bad74ba0777a4493ace28b415774e200e3fbead
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-13 17:03:54 -0700

    refactoring results-text.js

--- js/services/results-text.js
+++ js/services/results-text.js
@@ -1,46 +1,81 @@
+let results = [
 
-let personalFreedom = {
-    lowerHalf: 'you dont have much personal freedom',
-    upperHalf: 'you have lots of personal freedom'
+    { 
+        label: 'Personal Freedom',
+        key: 'personalFreedom',
+        thresholds: [
+            'you dont have much personal freedom',
+            'you have lots of personal freedom'
 
-};
+        ]
+    },
+    
+    { label: 'Economic Freedom',
+        key: 'economicFreedom',
+        thresholds: [
+            'you dont have much economic freedom',
+            'you have lots of economic freedom'
+        ]
+    },
+    
+    { label: 'Political Freedom',
+        key: 'politicalFreedom',
+        thresholds: [
+            'you dont have have much political freedom',
+            'you have lots of political freedom'
+        ]
+    
+    },
+    {
+        label: 'Approval',
+        key: 'approval',
+        thresholds: [
+            'you dont have have much approval',
+            'you have lots of approval'
+        ]
+    },
+    {
+        label:'Population',
+        key: 'population',
+        thresholds: [
+            'you dont have have much population',
+            'you have a large population'
+        ]
+    },
+    {
+        label:'EconProduction',
+        key: 'econProduction',
+        thresholds: [
+            'you dont have have much economic production',
+            'you have lots of economic production'
+        ]
+    },
+    {
+        label:'Health',
+        key:'health',
+        thresholds: [
+            'you  dont have have much healthcare',
+            'you have lots of healthcare'
+        ]
+    },
+    {
+        label:'Safety',
+        key: 'safety',
+        thresholds: [
+            'you  dont have have much safety',
+            'you have lots of safety'
+        ]
+    },
+    {
+        label:'Environment',
+        key: 'environment',
+        thresholds: [
+            'you  dont have have much environmental policies',
+            'you have lots of environmental policies'
+        ]
+    }
+];
 
-let economicFreedom = {
-    lowerHalf: 'you dont have much economic freedom',
-    upperHalf: 'you have lots of economic freedom'
-};
-
-let politicalFreedom = {
-    lowerHalf: 'you dont have have much political freedom',
-    upperHalf: 'you have lots of political freedom'
-
-};
-
-let approval = {
-    lowerHalf: 'you dont have have much approval',
-    upperHalf: 'you have lots of approval'
-};
-
-let population = {
-    lowerHalf: 'you dont have have much population',
-    upperHalf: 'you have a large population'
-};
-
-let econProduction = {
-    lowerHalf: 'you  dont have have much economic production',
-    upperHalf: 'you have lots of economic production'
-};
-
-let health = {
-    lowerHalf: 'you  dont have have much healthcare',
-    upperHalf: 'you have lots of healthcare'
-};
-let safety = {
-    lowerHalf: 'you  dont have have much saftey',
-    upperHalf: 'you have lots of saftey'
-}; 
-
-let environment = {
-    lowerHalf: 'you  dont have have much enviromental policies',
-    upperHalf: 'you have lots of enviromental policies'
+export default {
+    get: () => results
 };
