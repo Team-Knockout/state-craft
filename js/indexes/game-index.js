b017574995829d13c7076e4b7c6dbf0912255a1f
Jack <ajackofsometrades@gmail.com>
2018-08-14 08:51:22 -0700

    replacing 'game' name with survey

--- js/indexes/game-index.js
+++
@@ -1,7 +0,0 @@
-
-import App from '../apps/game-app.js';
-
-let root = document.getElementById('root');
-let app = new App();
-let dom = app.render();
-root.appendChild(dom);
--- js/layout/header.js
+++ js/layout/header.js
@@ -6,7 +6,7 @@ let template = function() {
     <nav>
         <ul>
             <li><a href="/index.html">Home</li>
-            <li><a href="/pages/game.html">Play Game</li>
+            <li><a href="/pages/survey.html">Play Game</li>
             <li><a href="/pages/results.html">Results</li>
         </ul>
     </nav>
