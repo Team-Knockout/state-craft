0f1a4e19159255291db0beacd8f4805bbc79e6ff
Jack <ajackofsometrades@gmail.com>
2018-08-15 12:05:15 -0700

    added some effects to question answers

--- js/services/question-api.js
+++ js/services/question-api.js
@@ -22,21 +22,24 @@ let questions = [
             answer: '"Compulsory voting makes about as much sense as having the death penalty for attempted suicide," says civil rights activist @@RANDOMNAME@@. "You can\'t force people to be free! You can only give them the choice. Besides, if all those derelicts who can\'t be bothered to get off their butts once every few years voted, who would they elect? I shudder to think."',
             response: 'Voting is voluntary.',
             effects: (nation) => {
-                nation.personalFreedom += 0.05;
+                nation.personalFreedom += 0.8;
+                nation.politicalFreedom += 0.3;
+                nation.health += 0.1;
+                nation.safety += 0.2;
             }
         },
         {
             answer: '"It\'s not contradictory at all," argues political commentator @@RANDOMNAME@@. "The fact is, if not everyone votes, the outcome isn\'t truly representative. Some groups—like elderly gun nuts—vote more often than others. That\'s why we always end up with such terrible politicians."',
             response: 'Voting is compulsory.',
             effects: (nation) => {
-                nation.personalFreedom -= 0.05;
+                nation.politicalFreedom -= 0.3;
             }
         },
         {
             answer: '"This raises an interesting issue," says @@RANDOMMALENAME@@, your brother. "And that is: why do we need elections, anyway? Seems to me it would be much simpler if you just decided what was right, and did it. Wouldn\'t that save everyone a lot of time?"',
             response: 'Elections have been outlawed.',
             effects: (nation) => {
-                nation.personalFreedom -= 0.05;
+                nation.politicalFreedom -= 1.1;
             }
         }] 
     },
@@ -46,19 +49,19 @@ let questions = [
         options:[{
             answer: '"There\'s a simple solution," says Pastor Felix, of the Catholic Church. "Divorce should be illegal. \'For better or worse,\' anyone remember how that goes? We should return to the good old days, when you got married for life and stuck by your partner no matter how much of a drunken, abusive, adulterating disappointment they turned out to be."',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: 'John Black, author of the hit book, \'Men are from Mars, Women are from Some Whole Other Place,\' has a simpler solution. "If couples would just call each other \'darling\' once in a while, there would be far fewer relationship breakdowns. A little affection is all it takes. So the government should make it mandatory: call your spouse \'darling\' at least once a day, or face a fine."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         },
         {
             answer: 'John Black, author of the hit book, \'Men are from Mars, Women are from Some Whole Other Place,\' has a simpler solution. "If couples would just call each other \'darling\' once in a while, there would be far fewer relationship breakdowns. A little affection is all it takes. So the government should make it mandatory: call your spouse \'darling\' at least once a day, or face a fine."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }] 
     },
@@ -68,19 +71,20 @@ let questions = [
         options: [{
             answer: '"People are sick of dirty, smelly automobiles," said protest organizer @@RANDOMNAME@@. "They\'re choking the city, the environment - our lives! Cars must be banned!"',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: '"The only thing people are sick of is long-haired idiots riding their bicycles at two miles an hour on major thoroughfares," says committed motorist @@RANDOMNAME@@. "People shouldn\'t be able to protest like this.The government needs to crack down on them."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
+                nation.politicalFreedom -= 0.1;
             }
         },
         {
             answer: 'The Automotive Manufacturers Association, meanwhile, has called for government support. "It\'s clear that we need to boost the level of automobile support in this country.This protest this morning is a clear indication of...um...anyway, we need more government funds."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -90,13 +94,13 @@ let questions = [
         options: [{
             answer: '"I quite enjoyed the book, until I got to the part where Harry summons evil demons to do his bidding," says religious leader @@RANDOMNAME@@. "Now that\'s just wrong.We need to restore some sense to this debate, by which I mean we should remove this book from the shelves, salt it thoroughly, and burn it."',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: 'Teachers union President @@RANDOMNAME@@ says, "Come on, the book is fantasy! And it\'s a damn good read.I\'d like the government to issue a statement of support for our teachers and librarians, so kids can enjoy good books without interference from religious wackos, like Christians."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -106,13 +110,13 @@ let questions = [
         options: [{
             answer: '"Good riddance!" says noted environmentalist @@RANDOMNAME@@. "Sniff that air! It\'s never been so clean!At last, our society is freeing itself from the consumer death-trap! I say it\'s time to take the final step and outlaw capitalism once and for all!"',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: '"This is a catastrophe waiting to happen," says the Chamber of Commerce. "Think of the consequences! Without big business, where do the jobs come from? Where do we get our medicine? Our cars? Our latest fashions? There are dozens of useless regulations the government could abolish today to make life easier for commerce, and it\'s high time they did."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -122,13 +126,13 @@ let questions = [
         options: [{
             answer: 'Social activist @@RANDOMNAME@@ is outraged. "Gambling needs to be outlawed immediately. It\'s no wonder children are becoming sucked into the vice, with adults setting such a poor example.Gambling is a stain on @@NAME @@international reputation and it must be stopped!"',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: 'However, Crown Casino chairperson @@RANDOMNAME@@ says, "What\'s wrong with children gambling? It prepares them for the realities of life, teaching them that success or failure is not due to hard work or intelligence, but the roll of the dice.Besides, if kids weren\'t gambling, they\'d be spray painting trains."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -138,19 +142,19 @@ let questions = [
         options: [{
             answer: 'Staunch traditionalist religious leader @@RANDOMNAME@@: boasts an excellent track record, having rapidly increased religious service attendances in @@HIS/HER@@ constituencies through the "Reaching God Through Guilt" program. Seen as a solid choice.',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: 'New Age thinker @@RANDOMNAME@@: a left-field candidate with some radical ideas. "For me, it\'s not about the name of your religion.It\'s about discovering your spirituality in whatever guise that takes. Some people call that a cult: I call it taking spirituality to the people."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         },
         {
             answer: 'Finally, there\'s @@RANDOMNAME@@. "If I am awarded the appointment, I will immediately resign," the ex-schoolteacher has declared. "Because, frankly, God is a big load of hokey. I\'ll be doing everyone a favor by just shutting up about it."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -160,25 +164,26 @@ let questions = [
         options: [{
             answer: '"These nuts have got to be stopped," demands concerned consumer @@RANDOMNAME@@. "They need to face the fact people want snack packs, no matter how many innocent chickens must be sacrificed. Besides, chickens would do the same to us if they had the chance."',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
+                nation.politicalFreedom -= 0.2;
             }
         },
         {
             answer: '"These Liberationists are highlighting an important issue," pleads @@RANDOMNAME@@. "Too often, animals are put through needless cruelty, just to make their flesh taste a little more deliciously succulent. I\'m sure we could ban the more horrific abuses without putting too much of a dent in our national obesity figures.Couldn\'t we?"',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         },
         {
             answer: '"Animals have feelings too!" yelled protestor @@RANDOMNAME@@, before being set upon by hungry passers-by. "Free the animals! Ban meat-eating!"',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         },
         {
             answer: 'Economist @@RANDOMNAME@@ has an alternative. "You don\'t need to take away the people\'s right to choose. You just need to build the costs of animal suffering into the price. A tax on meat-eating, in proportion to the amount of cruelty involved, would do the trick. Plus think of the benefit for the national coffers! Of course, poor people wouldn\'t be able to afford meat, but that\'s just more incentive for them to get jobs."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -188,19 +193,19 @@ let questions = [
         options: [{
             answer: '"For too long, our bodies have been trapped in these prisons of cotton and polyester!" yelled protester @@RANDOMNAME@@, while apparently developing a nasty case of sunburn. "We must repeal the puritanical laws that make public nudity a crime. My body—my choice to dangle!"',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: '"I agree," mused sociology professor @@RANDOMNAME@@. "But I don\'t think the protestors are going far enough.Public nudity shouldn\'t be an option: it should be compulsory. Nudity is highly liberating. And it would put that disgusting "Hooters" out of business once and for all."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         },
         {
             answer: '"Whoa, whoa," says noted accountant @@RANDOMNAME@@. "Are these people serious? The last thing I want to see when I\'m out for a coffee is some lumbering, over-weight nudist coming down the sidewalk toward me.If people want to get naked, they can do it in the privacy of their own homes.Think of the children!"',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
@@ -210,19 +215,19 @@ let questions = [
         options: [{
             answer: '"The fact is, the @@ANIMAL@@ population is out of control," says Beef-Based Agriculture spokesperson @@RANDOMNAME@@. "We have to do something about them anyway, so why not market them as tasty snacks? We could have @@ANIMAL@@ kebabs, @@ANIMAL@@ pies, @@ANIMAL@@-on-a-sticks—the possibilities are endless! Let’s not pass up this golden opportunity to provide a feast, if you will, for our economy."',
             effects: (nation) => {
-                nation.economicFreedom += 0.05;
+                nation.economicFreedom += 0.3;
             }
         },
         {
             answer: '"I agree that something needs to be done about @@ANIMAL@@ over-population," says random passer-by @@RANDOMNAME@@, "but eating them? That\'s kind of gross. Let\'s just shoot the ones we have to and shovel their bodies into ditches like normal."',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         },
         {
             answer: '"I am shocked and appalled!" declared SPCA President @@RANDOMNAME@@. "If anyone needs to be culled, it\'s us humans. The @@ANIMALPLURAL@@ were here first, remember? We need to take this as a sign to get our industry - agriculture in particular - to back off. The @@ANIMAL@@ is part of what makes @@NAME@@ a great nation!"',
             effects: (nation) => {
-                nation.economicFreedom -= 0.05;
+                nation.economicFreedom -= 0.3;
             }
         }]
     },
