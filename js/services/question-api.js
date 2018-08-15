889a50d7402f0f5333eff510400865a1ed276c7b
Jack <ajackofsometrades@gmail.com>
2018-08-15 14:07:32 -0700

    all answers have effects

--- js/services/question-api.js
+++ js/services/question-api.js
@@ -10,7 +10,7 @@
 To Do:
 - fix names with either dynamic input during question/answer creation, or create static names to use
 - add response component to display response text during survey. response text has been added to the first question in question api for reference of where it should go.
-
+- calculate approval at the end instead as a derived value?
 
 */
 
@@ -24,6 +24,7 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom += 0.8;
                 nation.politicalFreedom += 0.3;
+                nation.approval += 0.1,
                 nation.health += 0.1;
                 nation.safety += 0.2;
             }
@@ -34,6 +35,7 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom -= 1.1;
                 nation.politicalFreedom -= 0.3;
+                nation.safety -= 0.1;
             }
         },
         {
@@ -41,7 +43,7 @@ let questions = [
             response: 'Elections have been outlawed.',
             effects: (nation) => {
                 nation.personalFreedom += 2.0;
-                nation.politicalFreedom -= 1.1;
+                nation.politicalFreedom -= 2.1;
             }
         }] 
     },
@@ -52,18 +54,29 @@ let questions = [
             answer: '"There\'s a simple solution," says Pastor Felix, of the Catholic Church. "Divorce should be illegal. \'For better or worse,\' anyone remember how that goes? We should return to the good old days, when you got married for life and stuck by your partner no matter how much of a drunken, abusive, adulterating disappointment they turned out to be."',
             effects: (nation) => {
                 nation.personalFreedom -= 0.8;
+                nation.population += 2.0;
+                nation.econProduction += 0.4;
+                nation.health -= 0.1;
             }
         },
         {
             answer: 'John Black, author of the hit book, \'Men are from Mars, Women are from Some Whole Other Place,\' has a simpler solution. "If couples would just call each other \'darling\' once in a while, there would be far fewer relationship breakdowns. A little affection is all it takes. So the government should make it mandatory: call your spouse \'darling\' at least once a day, or face a fine."',
             effects: (nation) => {
                 nation.personalFreedom -= 0.4;
+                nation.population += 1.3;
+                nation.econProduction -= 0.1;
+                nation.health -= 0.1;
+                nation.safety += 0.1;
             }
         },
         {
             answer: '"There\'s a simple way to boost the marriage rate, " says gay rights activist @@RANDOMNAME@@. "Abolish those archaic laws that discriminate against same-sex marriages.It\'s obscene to treat people differently because of their sexual preference. Besides, everyone knows gay relationships are more stable than straight ones."',
             effects: (nation) => {
                 nation.personalFreedom += 0.8;
+                nation.population -= 0.8;
+                nation.econProduction += 0.1;
+                nation.health += 0.1;
+                nation.safety += 0.1;
             }
         }] 
     },
@@ -75,6 +88,11 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom -= 0.8;
                 nation.economicFreedom += 0.1;
+                nation.population -= 0.3;
+                nation.econProduction -= 0.7;
+                nation.health += 0.7;
+                nation.safety += 0.7;
+                nation.environment += 3.1;
             }
         },
         {
@@ -82,12 +100,18 @@ let questions = [
             effects: (nation) => {
                 nation.economicFreedom -= 0.4;
                 nation.politicalFreedom -= 0.1;
+                nation.econProduction += 0.2;
+                nation.environment -= 0.1;
             }
         },
         {
             answer: 'The Automotive Manufacturers Association, meanwhile, has called for government support. "It\'s clear that we need to boost the level of automobile support in this country.This protest this morning is a clear indication of...um...anyway, we need more government funds."',
             effects: (nation) => {
                 nation.economicFreedom -= 0.3;
+                nation.econProduction += 0.5;
+                nation.health -= 0.5;
+                nation.safety -= 0.3;
+                nation.environment -= 1.1;
             }
         }]
     },
@@ -97,15 +121,20 @@ let questions = [
         options: [{
             answer: '"I quite enjoyed the book, until I got to the part where Harry summons evil demons to do his bidding," says religious leader @@RANDOMNAME@@. "Now that\'s just wrong.We need to restore some sense to this debate, by which I mean we should remove this book from the shelves, salt it thoroughly, and burn it."',
             effects: (nation) => {
-                nation.personalFreedom += 0.7;
-                nation.economicFreedom += 0.1;
-                
+                nation.personalFreedom -= 0.7;
+                nation.economicFreedom -= 0.1;
+                nation.population += 0.4;
+                nation.econProduction += 0.1;
+                nation.safety += 0.1;
+                nation.environment -= 0.4;
             }
         },
         {
             answer: 'Teachers union President @@RANDOMNAME@@ says, "Come on, the book is fantasy! And it\'s a damn good read.I\'d like the government to issue a statement of support for our teachers and librarians, so kids can enjoy good books without interference from religious wackos, like Christians."',
             effects: (nation) => {
                 nation.personalFreedom += 1.3;
+                nation.population -= 0.05;
+                nation.environment += 0.3;
             }
         }]
     },
@@ -117,6 +146,11 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom -= 1.5;
                 nation.economicFreedom -= 0.4;
+                nation.population -= 1.8;
+                nation.econProduction -= 2.5;
+                nation.health += 0.3;
+                nation.safety += 0.3;
+                nation.environment += 2.5;
             }
         },
         {
@@ -124,6 +158,11 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom += 1.5;
                 nation.economicFreedom += 0.3;
+                nation.population -= 0.4;
+                nation.econProduction -= 0.4;
+                nation.health -= 0.2;
+                nation.safety -= 0.2;
+                nation.environment -= 1.1;
             }
         }]
     },
@@ -135,6 +174,10 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom -= 1.1;
                 nation.economicFreedom -= 0.3;
+                nation.population += 0.4;
+                nation.econProduction -= 0.2;
+                nation.health += 0.3;
+                nation.safety += 0.5;
             }
         },
         {
@@ -142,6 +185,11 @@ let questions = [
             effects: (nation) => {
                 nation.personalFreedom += 1.5;
                 nation.economicFreedom += 0.3;
+                nation.population -= 0.2;
+                nation.econProduction += 0.4;
+                nation.health -= 0.1;
+                nation.safety -= 0.8;
+                nation.environment -= 0.3;
             }
         }]
     },
@@ -151,21 +199,36 @@ let questions = [
         options: [{
             answer: 'Staunch traditionalist religious leader @@RANDOMNAME@@: boasts an excellent track record, having rapidly increased religious service attendances in @@HIS/HER@@ constituencies through the "Reaching God Through Guilt" program. Seen as a solid choice.',
             effects: (nation) => {
-                nation.economicFreedom += 0.3;
+                nation.economicFreedom -= 0.05;
+                nation.population += 0.7;
+                nation.econProduction -= 0.3;
+                nation.health -= 0.1;
+                nation.safety += 0.3;
+                nation.environment -= 0.8;
             }
         },
         {
             answer: 'New Age thinker @@RANDOMNAME@@: a left-field candidate with some radical ideas. "For me, it\'s not about the name of your religion.It\'s about discovering your spirituality in whatever guise that takes. Some people call that a cult: I call it taking spirituality to the people."',
             effects: (nation) => {
                 nation.personalFreedom += 0.4;
-                nation.economicFreedom -= 0.3;
+                nation.economicFreedom -= 0.05;
+                nation.population -= 0.7;
+                nation.econProduction += 0.7;
+                nation.health += 0.3;
+                nation.safety -= 0.3;
+                nation.environment += 0.7;
             }
         },
         {
             answer: 'Finally, there\'s @@RANDOMNAME@@. "If I am awarded the appointment, I will immediately resign," the ex-schoolteacher has declared. "Because, frankly, God is a big load of hokey. I\'ll be doing everyone a favor by just shutting up about it."',
             effects: (nation) => {
                 nation.personalFreedom += 0.2;
-                nation.economicFreedom += 0.2;
+                nation.economicFreedom += 0.1;
+                nation.population -= 0.1;
+                nation.econProduction += 0.1;
+                nation.health += 0.3;
+                nation.safety += 0.3;
+                nation.environment += 0.3;
             }
         }]
     },
@@ -176,29 +239,39 @@ let questions = [
             answer: '"These nuts have got to be stopped," demands concerned consumer @@RANDOMNAME@@. "They need to face the fact people want snack packs, no matter how many innocent chickens must be sacrificed. Besides, chickens would do the same to us if they had the chance."',
             effects: (nation) => {
                 nation.personalFreedom -= 0.4;
-                nation.economicFreedom += 0.3;
+                nation.economicFreedom += 0.2;
                 nation.politicalFreedom -= 0.2;
+                nation.econProduction += 0.2;
+                nation.environment -= 0.1;
             }
         },
         {
             answer: '"These Liberationists are highlighting an important issue," pleads @@RANDOMNAME@@. "Too often, animals are put through needless cruelty, just to make their flesh taste a little more deliciously succulent. I\'m sure we could ban the more horrific abuses without putting too much of a dent in our national obesity figures.Couldn\'t we?"',
             effects: (nation) => {
                 nation.personalFreedom -= 0.3;
-                nation.economicFreedom -= 0.3;
+                nation.economicFreedom -= 0.2;
+                nation.econProduction -= 0.1;
+                nation.health += 0.1;
+                nation.environment += 0.1;
             }
         },
         {
             answer: '"Animals have feelings too!" yelled protestor @@RANDOMNAME@@, before being set upon by hungry passers-by. "Free the animals! Ban meat-eating!"',
             effects: (nation) => {
-                nation.personalFreedom -= 0.5;
-                nation.economicFreedom -= 0.3;
+                nation.personalFreedom -= 1.4;
+                nation.economicFreedom -= 0.9;
+                nation.econProduction -= 0.7;
+                nation.health -= 0.1;
+                nation.environment += 0.5;
             }
         },
         {
             answer: 'Economist @@RANDOMNAME@@ has an alternative. "You don\'t need to take away the people\'s right to choose. You just need to build the costs of animal suffering into the price. A tax on meat-eating, in proportion to the amount of cruelty involved, would do the trick. Plus think of the benefit for the national coffers! Of course, poor people wouldn\'t be able to afford meat, but that\'s just more incentive for them to get jobs."',
             effects: (nation) => {
                 nation.personalFreedom -= 0.6;
-                nation.economicFreedom -= 0.3;
+                nation.economicFreedom -= 0.2;
+                nation.econProduction -= 0.4;
+                nation.environment += 0.3;
             }
         }]
     },
@@ -209,21 +282,31 @@ let questions = [
             answer: '"For too long, our bodies have been trapped in these prisons of cotton and polyester!" yelled protester @@RANDOMNAME@@, while apparently developing a nasty case of sunburn. "We must repeal the puritanical laws that make public nudity a crime. My body—my choice to dangle!"',
             effects: (nation) => {
                 nation.personalFreedom += 1.2;
-                nation.economicFreedom += 0.3;
+                nation.economicFreedom += 0.1;
+                nation.population -= 0.3;
+                nation.econProduction -= 0.4;
+                nation.health += 0.8;
+                nation.environment += 0.5;
             }
         },
         {
             answer: '"I agree," mused sociology professor @@RANDOMNAME@@. "But I don\'t think the protestors are going far enough.Public nudity shouldn\'t be an option: it should be compulsory. Nudity is highly liberating. And it would put that disgusting "Hooters" out of business once and for all."',
             effects: (nation) => {
                 nation.personalFreedom -= 0.7;
-                nation.economicFreedom -= 0.3;
+                nation.economicFreedom += 0.2;
+                nation.population -= 0.5;
+                nation.econProduction -= 0.6;
+                nation.health += 1.1;
+                nation.environment += 0.5;
             }
         },
         {
             answer: '"Whoa, whoa," says noted accountant @@RANDOMNAME@@. "Are these people serious? The last thing I want to see when I\'m out for a coffee is some lumbering, over-weight nudist coming down the sidewalk toward me.If people want to get naked, they can do it in the privacy of their own homes.Think of the children!"',
             effects: (nation) => {
                 nation.personalFreedom -= 0.6;
-                nation.economicFreedom -= 0.3;
+                nation.population += 0.2;
+                nation.econProduction -= 0.05;
+                nation.health -= 0.2;
             }
         }]
     },
@@ -234,22 +317,35 @@ let questions = [
             answer: '"The fact is, the @@ANIMAL@@ population is out of control," says Beef-Based Agriculture spokesperson @@RANDOMNAME@@. "We have to do something about them anyway, so why not market them as tasty snacks? We could have @@ANIMAL@@ kebabs, @@ANIMAL@@ pies, @@ANIMAL@@-on-a-sticks—the possibilities are endless! Let’s not pass up this golden opportunity to provide a feast, if you will, for our economy."',
             effects: (nation) => {
                 nation.personalFreedom += 0.1;
-                nation.economicFreedom += 0.3;
-                
+                nation.economicFreedom += 0.1;
+                nation.population += 0.2;
+                nation.econProduction += 0.1;
+                nation.health += 0.1;
+                nation.safety += 0.1;
+                nation.environment += 0.3;
             }
         },
         {
             answer: '"I agree that something needs to be done about @@ANIMAL@@ over-population," says random passer-by @@RANDOMNAME@@, "but eating them? That\'s kind of gross. Let\'s just shoot the ones we have to and shovel their bodies into ditches like normal."',
             effects: (nation) => {
                 nation.personalFreedom += 0.2;
-                nation.economicFreedom -= 0.3;
+                nation.economicFreedom += 0.05;
+                nation.econProduction += 0.3;
+                nation.health -= 0.2;
+                nation.safety -= 0.2;
+                nation.environment -= 0.6;
             }
         },
         {
             answer: '"I am shocked and appalled!" declared SPCA President @@RANDOMNAME@@. "If anyone needs to be culled, it\'s us humans. The @@ANIMALPLURAL@@ were here first, remember? We need to take this as a sign to get our industry - agriculture in particular - to back off. The @@ANIMAL@@ is part of what makes @@NAME@@ a great nation!"',
             effects: (nation) => {
                 nation.personalFreedom -= 0.4;
-                nation.economicFreedom -= 0.3;
+                nation.economicFreedom -= 0.1;
+                nation.population -= 0.5;
+                nation.econProduction -= 0.3;
+                nation.health += 0.1;
+                nation.safety += 0.1;
+                nation.environment += 0.4;
             }
         }]
     },
